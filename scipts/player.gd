091b9f58f0bcca37a84717594a79a9264c02fcad
omatheusd <matheus.defaveri21@gmail.com>
2025-02-06 22:23:10 -0300

    Add files via upload

--- scipts/player.gd
+++ scipts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-
+var speedx = 100
 const speed = 300.0
 const forca_pulo = -400.0
 
@@ -9,14 +9,18 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	if position.x <= 15:
+		position.x = 15
+	if position.x >= 1137:
+		position.x = 1137
+	#print(is_on_floor())
 	# Handle jump.
 	if Input.is_action_just_pressed("space") and is_on_floor():
 		velocity.y = forca_pulo
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("esquerda(player1)", "direita(player1)")
 	if direction:
 		velocity.x = direction * speed
 	else:
